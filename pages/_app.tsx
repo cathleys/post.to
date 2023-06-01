f45902a18163c29d05340b6a160d3ce7e8599eed
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-06-01 12:46:25 +0800

    Changes to App router

--- pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import "../styles/Home.module.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
