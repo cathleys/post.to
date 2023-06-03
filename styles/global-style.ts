a718ef75a4acf9be370cbd6d97e36a21266ebb29
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-06-03 12:03:01 +0800

    adds hover effect global style anchor

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -5,13 +5,18 @@ ${normalize}
   html,
     body {
         font-family: 'Inter Variable', sans-serif;
-     
+        color: #1d2939;
     }
 
     a {
-        color: #1d2939;
+       
         text-decoration: none;
         cursor: pointer;
+
+        &:hover {
+           color: rgba(108, 122, 137) ;
+         
+        }
     }
 
     li {
