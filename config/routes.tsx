2af0dfd03d721cfe8c346bcaf2b806aa31814c6f
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-06-06 16:23:53 +0800

    adds user menu links

--- config/routes.tsx
+++ config/routes.tsx
@@ -2,6 +2,7 @@ export const Routes = {
   home: "/",
   signup: "/sign-up",
   login: "/login",
+  logout: "/logout",
   createPost: "/create-post",
   settings: "/settings",
   singlePost: "/single-post",
