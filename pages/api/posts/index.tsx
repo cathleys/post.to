6b968e1cf201e71393abefe9b498f886eef122fd
cath14 <108257596+cathleys@users.noreply.github.com>
2023-06-10 18:31:51 +0800

    Sets up DB connection (#21)

---
+++ pages/api/posts/index.tsx
@@ -0,0 +1,36 @@
+import connectDB from "@utils/connect-db";
+import Post from "@models/post";
+
+connectDB();
+
+async function Posts(req: any, res: any) {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const posts = Post.find({});
+
+        res.status(200).json({ success: true, data: posts });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    case "POST":
+      try {
+        const post = Post.create(req.body);
+
+        res.status(200).json({ success: true, data: post });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
+
+export default Posts;
