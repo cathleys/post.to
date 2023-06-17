0aa0b7b4afdb0cf641fbe38d573847a6c5176469
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-06-17 17:52:33 +0800

    now i can rest my head on your shoulder

--- lib/load-posts.ts
+++ lib/load-posts.ts
@@ -2,8 +2,7 @@ import fetch from "isomorphic-fetch";
 
 export async function loadPosts() {
   // Call an external API endpoint to get posts
-  const baseUrl =
-    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1";
   const res = await fetch(`${baseUrl}/api/posts`);
   const { data } = await res.json();
 
