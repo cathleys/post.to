516bffdfb6a79d972338c0e21155c15439f2c6ef
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-06-17 17:56:59 +0800

    and throw out all the things

--- lib/load-posts.ts
+++ lib/load-posts.ts
@@ -2,7 +2,7 @@ import fetch from "isomorphic-fetch";
 
 export async function loadPosts() {
   // Call an external API endpoint to get posts
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1";
+  const baseUrl = "http://127.0.0.1";
   const res = await fetch(`${baseUrl}/api/posts`);
   const { data } = await res.json();
 
