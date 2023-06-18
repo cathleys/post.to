dcec5db7f0e4369113171e2f03deb47f64169877
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-06-18 12:52:15 +0800

    80801

--- lib/load-posts.ts
+++ lib/load-posts.ts
@@ -1,10 +1,9 @@
 import fetch from "isomorphic-fetch";
 
 export async function loadPosts() {
-  // Call an external API endpoint to get posts
-  // const baseUrl =
-  //   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
-  const res = await fetch("http://localhost:3000/api/posts");
+  const baseUrl =
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+  const res = await fetch(`${baseUrl}/api/posts`);
   const { data } = await res.json();
 
   return data;
