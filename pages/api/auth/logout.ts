f2de3dc29044ad347813c50a23012a2cde79b62c
cath14 <108257596+cathleys@users.noreply.github.com>
2023-07-10 19:27:36 +0800

    db connection (#27)

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
+import db from "@utils/db";
 const logout = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
+    await db();
     res.setHeader(
       "Set-Cookie",
       "token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"
--- pages/api/auth/user-profile.ts
+++ pages/api/auth/user-profile.ts
@@ -1,7 +1,9 @@
 import type { NextRequest } from "next/server";
 import jwt, { Secret } from "jsonwebtoken";
+import db from "@utils/db";
 
 const userProfile = async (req: NextRequest, res: any) => {
+  await db();
   const secret: Secret | undefined = process.env.JWT_SECRET || "";
   if ("token" in req.cookies) {
     const { token } = req.cookies;
